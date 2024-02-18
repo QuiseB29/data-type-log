04b3765e60cbb6f5b420bfcdb38e742a56fd54a3
QuiseB29 <bmarquise544@gmail.com>
2024-02-17 21:20:38 -0600

    Data Types

---
+++ datat.py
@@ -0,0 +1,9 @@
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+print(variable_a, type(variable_a))
+print(variable_b, type(variable_b))
+print(variable_c, type(variable_c))
+print(variable_d, type(variable_d))
